76c96c1462c926fa9a733e86460efeb0beef5bbd
BuilesYeison <y.builes.uribe@gmail.com>
2021-04-29 10:43:42 -0500

    Delete selected item from academicInfo array

--- src/app/form/academic/academic.component.ts
+++ src/app/form/academic/academic.component.ts
@@ -7,19 +7,25 @@ import {FormControl,FormGroup,Validators} from '@angular/forms'
   styleUrls: ['./academic.component.scss']
 })
 export class AcademicComponent implements OnInit {
-  academicInfo:any = []
+  academicInfo:any = [] //all academic info 
   academicForm = new FormGroup({
     place:new FormControl("",[Validators.required]),
-    duration:new FormControl("",[Validators.required,Validators.pattern("[0-9]*")]),
-    year:new FormControl("",[Validators.required,Validators.pattern("[0-9]{4}")]),
+    duration:new FormControl("",[Validators.required,Validators.pattern("[0-9]*")]),//only numbers
+    year:new FormControl("",[Validators.required,Validators.pattern("[0-9]{4}")]),//only four numbers
     title:new FormControl("",[Validators.required])
   });
   constructor() { }
   pushItem():void{
-    this.academicInfo.push(this.academicForm.value)
-    console.log(this.academicInfo);
+    this.academicInfo.push(this.academicForm.value);//push an object when the submit button is pressed
   }
-  get f(){return this.academicForm.controls}
+  deleteItem(item:any):void{//delete item when char x is pressed in the header
+    for(let i = 0;i<this.academicInfo.length;i++){
+      if(this.academicInfo[i].title === item.title){
+        this.academicInfo.splice(i,1);
+      }
+    }
+  }
+  get f(){return this.academicForm.controls} //get the states of controls
   ngOnInit(): void {
   }
 
