095519e155009759e751abbad695c2b374953fb1
BuilesYeison <y.builes.uribe@gmail.com>
2021-04-29 08:05:22 -0500

    Documented

--- src/app/form/certificaciones/certificaciones.component.ts
+++ src/app/form/certificaciones/certificaciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{FormGroup,FormControlName, FormControl, Validators} from '@angular/forms' //reactive forms libraries required
 
 @Component({
   selector: 'app-certificaciones',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certificaciones.component.scss']
 })
 export class CertificacionesComponent implements OnInit {
-
+  certificados:any = []; //will save all certificates
+  certificadosForm = new FormGroup({
+    certificado:new FormControl("",[Validators.required]),
+    year:new FormControl("",[Validators.required,Validators.pattern("[0-9]{4}")])//only four numbers
+  });
+  pushCertificado():void{//when submit add a new certificate
+    this.certificados.push(this.certificadosForm.value)
+  }
+  deleteItem(item:any):void{//when press x key in the item card delete that item
+    for(let i= 0;i<this.certificados.length;i++){
+      if(this.certificados[i].certificado === item.certificado){
+       this.certificados.splice(i,1)
+      }
+    }
+  }
+  get f(){
+    return this.certificadosForm.controls
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,10 +8,10 @@ import{FormGroup,FormControlName, FormControl,Validators} from '@angular/forms'
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  model!: NgbDateStruct;
+  model!: NgbDateStruct; //for bootstrap datepicker
   username:string =""
   userInfoForm=new FormGroup({//reactive form
-    name:new FormControl("",[Validators.required,Validators.pattern('[A-Za-z ]*')]),
+    name:new FormControl("",[Validators.required,Validators.pattern('[A-Za-z ]*')]),//only words with spaces
     ID:new FormControl("",[Validators.required,Validators.pattern("[0-9]*"),Validators.maxLength(10),Validators.minLength(5)]),
     cargo:new FormControl("", [Validators.required]),
     TP:new FormControl(""),
@@ -20,7 +20,7 @@ export class FormComponent implements OnInit {
   });
 
   get f(){
-    return this.userInfoForm.controls//init the control for get their states
+    return this.userInfoForm.controls//init the control to get their states
   }
   getUsername(){
     //return the first name the user introduce!
