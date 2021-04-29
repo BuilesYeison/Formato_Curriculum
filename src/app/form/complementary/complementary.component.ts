c39cc425bbd492312230dba2aee4ad981bf1d4a4
BuilesYeison <y.builes.uribe@gmail.com>
2021-04-29 14:11:06 -0500

    Complementary knowlege:Form design, push info and validations done

--- src/app/form/complementary/complementary.component.ts
+++ src/app/form/complementary/complementary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators}from "@angular/forms"
 
 @Component({
   selector: 'app-complementary',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complementary.component.scss']
 })
 export class ComplementaryComponent implements OnInit {
-
-  constructor() { }
-
+  complementary:any=[]
+  complementaryForm = new FormGroup({
+    place:new FormControl("", [Validators.required]),
+    duration:new FormControl("", [Validators.required,Validators.pattern("[0-9]*")]),
+    year:new FormControl("", [Validators.required, Validators.pattern("[0-9]{4}")]),
+    description:new FormControl("", [Validators.required])
+  });
+  pushItem():void{
+    this.complementary.push(this.complementaryForm.value);
+  }
+  get f(){return this.complementaryForm.controls}
   ngOnInit(): void {
   }
 
